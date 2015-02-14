f1dec776f1c4f70d08abd00140bfed98f37701a6
mtdmn <mtdmn0@gmail.com>
2015-02-14 23:02:38 +0900

    upload works finallygit add script.js

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 var Zap = {
     upload_pre_write: function(bundle) {
-
-        var separator = "f94636b7375c4a37862029d4dc8bafe7";
-        bundle.request.headers.Accept = '*/*';
-        bundle.request.headers['Content-type'] = "multipart/form-data; boundary=" + separator;
-        bundle.request.headers['Content-length'] = bundle.action_fields_full.gpx.length;
-        bundle.request.headers['User-Agent'] = "hogehoge/1.0.0";
-        bundle.request.data = "--"+separator+"\r\n" +
-            "Content-Disposition: form-data; name=\"activity_type\"\r\n"+
-            "\r\n"+
-            "running\r\n"+
-            "--"+separator+"\r\n" +
-            "Content-Disposition: form-data; name=\"file_to_upload\"; filename=\"file.gpx\"\r\n"+
-            "Content-Type: application/octet-stream\r\n"+
-            "\r\n"+
-            bundle.action_fields_full.gpx +
-            "--"+separator+"--\r\n";
- 
-      console.log(bundle.request.data);
-         return bundle.request;
-
+        bundle.request.files =  { "file": [ "file.gpx", bundle.action_fields_full.gpx, "application/octet-stream" ] } ;
+    
+        bundle.request.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        // leave request data as object, not string!
+        bundle.request.data = JSON.parse( '{ "activity_type": "running", "data_type": "gpx" }' );
+        // we will mix request.data and request.files together
+        return bundle.request; // let zapier complete it
     }
 
 };
